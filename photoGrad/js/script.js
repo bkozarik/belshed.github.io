8487ac91e390566f8240b7cb30683dd78dfd45d7
Belshed <sercom92@gmail.com>
2020-05-05 15:56:16 +0300

    js

--- photoGrad/js/script.js
+++ photoGrad/js/script.js
@@ -17,4 +17,37 @@ $(document).ready(function(){
     $('.comments__slider').on('afterChange', ()=>{
         $('.comments__counter')[0].innerText = Number($('.comments__item.slick-current.slick-active')[0].attributes[1].value) + 1 + '/6';
     });
+    $('#help').on('click', () => {
+        if($('.header__popup-login').css('display') != 'block'){
+            if($('.header__popup-wrap').css('display') == 'none'){
+                $('.header__popup-wrap').css('display', 'block');
+                $('.header__popup').css('display', 'block');
+                $('.header__popup').css('transform', 'translateY(0%)');
+            }
+            else{
+                $('.header__popup-wrap').css('display', 'none');
+                $('.header__popup').css('display', 'none');
+                $('.header__popup').css('transform', 'translateY(-100%)');
+            }
+        } 
+    });
+    $('#login').on('click', () => {
+        if($('.header__popup').css('display') != 'block'){
+            if($('.header__popup-wrap').css('display') == 'none'){
+                $('.header__popup-wrap').css('display', 'block');
+                $('.header__popup-login').css('display', 'block');
+                $('.header__popup-login').css('transform', 'translate(0%, 0%)');
+            }
+            else{
+                $('.header__popup-wrap').css('display', 'none');
+                $('.header__popup-login').css('display', 'none');
+                $('.header__popup-login').css('transform', 'translateY(0%, -100%)');
+            }
+    }
+    });
+    $('.header__popup-wrap').on('click', () => {
+        $('.header__popup-wrap').css('display', 'none');
+        $('.header__popup-login').css('display', 'none');
+        $('.header__popup').css('display', 'none');
+    });
 });
