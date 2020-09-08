2ae5abd06a1a29dcba0a5af57b2d14663f3786df
Belshed <sercom92@gmail.com>
2020-09-08 13:32:50 +0300

    New site!!!!

--- dentko/js/main.js
+++ dentko/js/main.js
@@ -4,6 +4,94 @@ document.addEventListener('DOMContentLoaded', () => {
     let popups = document.querySelectorAll('.popup');
     let popupSucsessTrigger = document.querySelectorAll('.popup-sucsess-open');
     let menuBtn = document.querySelector('.menu-open');
+    let sliders = document.querySelectorAll('.uk-slider-container');
+    let spoilerLink = document.querySelector('.spoiler-link');
+
+    const toggleSpoiler = () => {
+        event.preventDefault();
+
+        let parent = event.target.parentNode;
+
+        while(!parent.classList.contains('spoiler-text')){
+            parent = parent.parentNode
+        }
+
+        let textContainer = parent.querySelector('.about__text-wrap');
+
+        let textContainerChildren = Array.from(textContainer.children);
+
+        let totalHeight = 200;
+
+        textContainerChildren.forEach(item => {
+            totalHeight += item.offsetHeight;
+        });
+
+        console.log(totalHeight);
+
+        textContainer.classList.toggle('open');
+        if(textContainer.classList.contains('open')){
+            textContainer.style.maxHeight = totalHeight + "px";
+        }
+        else{
+            textContainer.removeAttribute('style');
+        }
+    }
+
+    const updSlidersPagination = () => { // Пагинация у слайдеров
+        try{
+            sliders.forEach(slider => {
+                try{
+                    let paginationTotal = slider.querySelectorAll('.slider__pagination-total');
+                    let sliderItems = slider.querySelectorAll('.uk-slider-items li');
+                    let sliderItemsActive = slider.querySelectorAll('li.uk-active');
+    
+                    paginationTotal.forEach(item => {
+                        item.innerText = sliderItems.length - sliderItemsActive.length + 1;
+                    });
+
+                    let counter = 0,
+                        activePos;
+
+                    sliderItems.forEach(item => {
+                        if(item.classList.contains('uk-active') && activePos == undefined){
+                            activePos = counter;
+                        }
+                        counter += 1;
+                    });
+                    let paginationActive = slider.querySelectorAll('.slider__pagination-active');
+                    paginationActive.forEach(span => {
+                        span.innerText = activePos + 1;
+                    });
+
+                }   
+                catch{}
+    
+                UIkit.slider(slider, {finite: true});
+
+                slider.addEventListener('itemshow', () => {
+                    let target = event.target;
+                    let targetWrap = target.parentNode;
+
+                    let sliderItems = targetWrap.querySelectorAll('li');
+                    let counter = 0,
+                        activePos;
+
+                    sliderItems.forEach(item => {
+                        if(target == item){
+                            activePos = counter;
+                        }
+                        counter += 1;
+                    });
+
+                    let paginationActive = slider.querySelectorAll('.slider__pagination-active');
+                    paginationActive.forEach(span => {
+                        span.innerText = activePos + 1;
+                    });
+                });
+            });
+        }
+        catch{}
+    }
 
     const toggleMenu = () => {
         event.preventDefault();
@@ -87,6 +175,60 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    try{
+        let galleryThumbs = new Swiper('.gallery-thumbs', {
+            spaceBetween: 30,
+            slidesPerView: 3,
+            updateOnWindowResize: true,
+            freeMode: true,
+            direction: 'vertical',
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+        });
+    
+        let galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 30,
+        direction: 'vertical',
+        slidesPerView: 1,
+        updateOnWindowResize: true,
+        loop: true,
+        navigation: {
+            nextEl: 'a.swiper.slider-next',
+            prevEl: 'a.swiper.slider-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
+        });
+
+        let reviewsSlider = document.querySelector('.reviews__slider_main').swiper;
+        document.querySelector('.swiper__pagination-total').innerText = reviewsSlider.slides.length - 2;
+        reviewsSlider.on('slideChange', () => {
+            document.querySelector('.swiper__pagination-active').innerText = reviewsSlider.realIndex + 1;
+        });
+    }
+    catch{}
+
+    try{
+        let aboutSlider = new Swiper('.about__slider-container', {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            navigation: {
+                nextEl: '.about__slider-next',
+                prevEl: '.about__slider-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'progressbar',
+            },
+        });
+    }
+    catch{}
+    try{
+        spoilerLink.addEventListener('click', toggleSpoiler);
+    }
+    catch{}
+
     menuBtn.addEventListener('click', toggleMenu);
 
     popupTriggers.forEach(item => {
@@ -109,6 +251,12 @@ document.addEventListener('DOMContentLoaded', () => {
         mobileCheck();
     });
 
+    try{
+        document.querySelector('.uk-accordion-title').click();
+    }
+    catch{}
+
     mobileCheck();
     scrollHandler();
+    updSlidersPagination();
 });
