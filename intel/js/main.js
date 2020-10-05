c4381b49ff81231ca1f20b3cea2c6bce39a3d660
Belshed <sercom92@gmail.com>
2020-10-05 20:25:20 +0300

    intel Done

---
+++ intel/js/main.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let scrollLinks = document.querySelectorAll('.js-scroll-link');
+    let menuTriggers = document.querySelectorAll('.js-menu-trigger');
+    let menu = document.querySelector('.js-menu');
+
+    const scrollLinkClick = () => {
+        event.preventDefault();
+
+        let link = event.target;
+        let linkHref = link.getAttribute('href');
+
+        document.querySelector(linkHref).scrollIntoView({
+            block: 'start',
+            behavior: 'smooth',
+        });
+
+        toggleMenu(false);
+    }
+
+    const toggleMenu = (state = null) => {
+        let menuBtn = document.querySelector('.burger.js-menu-trigger');
+
+        typeof(state) == 'object' ? state = null : state = state;
+        
+        if(state == true){
+            menuBtn.classList.add('active');
+            menu.classList.add('active');
+        }
+        else if(state == false){
+            menuBtn.classList.remove('active');
+            menu.classList.remove('active');
+        }
+        else{
+            menuBtn.classList.toggle('active');
+            menu.classList.toggle('active');
+        }
+    }
+
+    const resizeHandler = () => {
+        if(window.innerWidth <= 940){
+            menu.appendChild(document.querySelector('.nav'));
+        }
+        else{
+            document.querySelector('.header__wrap').appendChild(document.querySelector('.nav'));
+            toggleMenu(false);
+        }
+    }
+
+    scrollLinks.forEach(link => link.addEventListener('click', scrollLinkClick));
+
+    menuTriggers.forEach(trgger => trgger.addEventListener('click', toggleMenu));
+
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
+});
