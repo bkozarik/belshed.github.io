9f31d9fdf872c2e9ed14f3695af940d4e8193a2a
Belshed <sercom92@gmail.com>
2020-10-24 23:44:45 +0300

    dsa

--- braun/js/main.js
+++ braun/js/main.js
@@ -2,15 +2,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerBtn = document.querySelector('.js-burger');
     const menuItems = document.querySelectorAll('.js-menu-item');
 
-    const burgerBtnClick = () => {
-        burgerBtn.classList.toggle('active');
+    const toggleMenu = (state = null) => {
+        if(typeof(state) == 'object'){
+            return () => {
+                burgerBtn.classList.toggle('active');
 
-        menuItems.forEach((item, index) => {
-            item.classList.toggle('active');
-            item.style.top = item.classList.contains('active') ? document.querySelector('.header').getBoundingClientRect().height + index * item.getBoundingClientRect().height + "px" : "0px";
-        });
+                menuItems.forEach((item, index) => {
+                    item.classList.toggle('active');
+                    item.style.top = item.classList.contains('active') ? document.querySelector('.header').getBoundingClientRect().height + index * item.getBoundingClientRect().height + "px" : "0px";
+                });
+
+                document.addEventListener('click', () => {
+                    let target = event.target;
+        
+                    if(!target.classList.contains('js-menu-item') && !target.classList.contains('js-burger')){
+                        toggleMenu(false);
+                    }
+                });
+            }
+        }
+        else{
+            state ? burgerBtn.classList.add('active') : burgerBtn.classList.remove('active');
+
+            menuItems.forEach((item, index) => {
+                state ? item.classList.add('active') : item.classList.remove('active');
+                item.style.top = state ? document.querySelector('.header').getBoundingClientRect().height + index * item.getBoundingClientRect().height + "px" : '0px';
+            });
+        }
     }
 
-    burgerBtn.addEventListener('click', burgerBtnClick);
+    new WOW().init();
+
+    burgerBtn.addEventListener('click', toggleMenu());
 
 });
