22d227be469fc03e741f41081236606813efdbb5
Belshed <sercom92@gmail.com>
2020-10-24 19:49:01 +0300

    wip

--- braun/js/main.js
+++ braun/js/main.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const burgerBtn = document.querySelector('.js-burger');
+    const menuItems = document.querySelectorAll('.js-menu-item');
 
-    
+    const burgerBtnClick = () => {
+        burgerBtn.classList.toggle('active');
+
+        menuItems.forEach((item, index) => {
+            item.classList.toggle('active');
+            item.style.top = item.classList.contains('active') ? document.querySelector('.header').getBoundingClientRect().height + index * item.getBoundingClientRect().height + "px" : "0px";
+        });
+    }
+
+    burgerBtn.addEventListener('click', burgerBtnClick);
 
 });
