5dbe2a3508acc8a334320534e955061231ce0838
Belshed <sercom92@gmail.com>
2020-11-04 02:58:44 +0300

    fsdfs

--- citadel/js/main.js
+++ citadel/js/main.js
@@ -1,23 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
     const languageCheckbox = document.querySelectorAll('.js-language-radio');
     const scrollLinks = document.querySelectorAll('.js-scroll-link');
+    
+    const projects = document.querySelector('.js-projects');
+    const projectsBtn = document.querySelector('.js-projects-button');
 
-    let mainSwiperNode;
+    const burger = document.querySelector('.js-burger');
+    const menu = document.querySelector('.js-menu');
+
+    let mainSwiper, mainSwiperNode;
 
     const languageCheckboxChange = () => {
 
+    }
+
+    const toggleMenu = (state = null) => {
+        if(typeof(state) == 'object'){
+            return () => {
+                menu.classList.toggle('active');
+                burger.classList.toggle('active');
+            };
+        }
+        else{
+            state ? menu.classList.add('active') : menu.classList.remove('active');
+            state ? burger.classList.add('active') : burger.classList.remove('active');
+        }
     }
 
     const scrollLinkClick = () => {
         event.preventDefault();
 
         const targetLink = event.target;
-        const targetPage = parseInt(targetLink.dataset.page) - 1;
-
+    
         scrollLinks.forEach(link => link.classList.remove('active'));
         targetLink.classList.add('active');
 
-        mainSwiperNode.slideTo(targetPage, 500, true);
+        if(mainSwiper.classList.contains('swiper-container-initialized')){
+            const targetPage = parseInt(targetLink.dataset.page) - 1;
+    
+            mainSwiperNode.slideTo(targetPage, 500, true);
+        }
+        else{
+            let href;
+            
+            console.log(targetLink);
+
+            href = targetLink.getAttribute('href');
+            
+            window.scrollTo({
+                top: document.querySelector(href).offsetTop + 30,
+                behavior: 'smooth'
+            });
+        }
     }
 
     const switchScrollLink = index => {
@@ -43,8 +77,35 @@ document.addEventListener('DOMContentLoaded', () => {
         swiper.on('slideChange', () => switchScrollLink(swiper.activeIndex));
     }
 
+    const resizeHandler = () => {
+        if(window.innerWidth < 770){
+            mainSwiper = document.querySelector('.js-main-swiper');
+
+            if(mainSwiper.classList.contains('swiper-container-initialized')){
+                mainSwiperNode.destroy();
+            }
+        }
+        else{
+            mainSwiper = document.querySelector('.js-main-swiper');
+
+            if(!mainSwiper.classList.contains('swiper-container-initialized')){
+                mainSwiperInit();
+            }
+        }
+    }
+
+    const projectsBtnClick = () => {
+        projects.classList.toggle('active');
+
+        projectsBtn.innerText = projects.classList.contains('active') ? "Скрыть проекты" : "Показать все проекты";
+    }
+
+    burger.addEventListener('click', toggleMenu());
+    projectsBtn.addEventListener('click', projectsBtnClick);
     scrollLinks.forEach(link => link.addEventListener('click', scrollLinkClick));
     languageCheckbox.forEach(checkbox => checkbox.addEventListener('input', languageCheckboxChange));
 
-    mainSwiperInit();
+    window.addEventListener('resize', resizeHandler);
+
+    resizeHandler();
 });
