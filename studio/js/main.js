d87ada8ddd31f8748fc7bfd4f0e1a4aeeda56972
Belshed <sercom92@gmail.com>
2020-12-28 15:39:11 +0300

    desctop done

--- studio/js/main.js
+++ studio/js/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header');
     const dropdownItems = document.querySelectorAll('.js-dropdown');
+
+    const mouseMoveElements = document.querySelectorAll('.js-mouse-move');
+    const parallaxElements = document.querySelectorAll('.js-parallax');
+
+    const forms = document.querySelectorAll('form');
+    const mainForm = document.querySelector('.js-main-form');
+    const popupForm = document.querySelector('.js-popup-form');
+    const telInputs = document.querySelectorAll('input[type="tel"]');
+
+    const popupTriggers = document.querySelectorAll('.js-toggle-popup');
+    const popupCloseTriggers = document.querySelectorAll('.js-popup-close');
+    const popups = document.querySelectorAll('.js-popup');
+    let logosSwiper_swiper;
     
     const logosSwiperInit = () => {
         const logosSwiper = document.querySelector('.js-logos-swiper');
@@ -10,7 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
             freeMode: true,
             loop: true,
             grabCursor: true,
-        })
+        });
+
+        logosSwiper_swiper = logosSwiper.swiper;
     }
 
     const dropdownSelectInit = (dropdownItems) => {
@@ -116,10 +131,50 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    const countUp = () => {
+        document.querySelectorAll('.js-count-up').forEach(item => {
+
+            let target = parseInt(item.dataset.target);
+            let count = 0
+            const increment = () => { 
+                let [_, num, suffix] = String(count).match(/^(.*?)((?:[,.]\d+)?|)$/);
+                
+                let string = num.replace(/\B(?=(?:\d{3})*$)/g, ' ');
+
+                item.innerHTML = item.dataset.format.replace('-/-', string);
+
+                count += target < 1000 ? 1 : 2500;
+
+                if(count <= target){
+                    setTimeout(increment, parseInt(item.dataset.time) / target);
+                }
+            }
+
+            increment();
+        });
+    }
+
+    const parallax = () => {
+        parallaxElements.forEach(element => {
+            let itemYCoord = element.getBoundingClientRect().y * 100;
+
+            if(element.dataset.speed) itemYCoord *= parseFloat(element.dataset.speed);
+
+            if(element.dataset.center == "true"){
+                element.style.transform = `translate(-50%, ${(-itemYCoord / window.innerHeight) - 50}%`;
+            }
+            else{
+                element.style.transform = `translate(0, ${-itemYCoord / window.innerHeight}%`;
+            }
+        });
+    }
+
     const scrollHandler = () => {
-        window.pageYOffset > 6 ? header.classList.add('fixed') : header.classList.remove('fixed');
+        window.pageYOffset > 60 ? header.classList.add('fixed') : header.classList.remove('fixed');
 
-
+        parallax();
+        
+        logosSwiper_swiper.setTranslate(-1 * window.pageYOffset * 0.5);
     }
 
     const articlesSwiperInit = () => {
@@ -136,6 +191,48 @@ document.addEventListener('DOMContentLoaded', () => {
                 prevEl: '.articles__control_prev',
                 nextEl: '.articles__control_next',
             }
+        });
+    }
+
+    const maskInit = () => {
+
+        const setCursorPosition = (pos, elem) => {
+            elem.focus();
+            if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+            else if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.collapse(true);
+                range.moveEnd("character", pos);
+                range.moveStart("character", pos);
+                range.select();
+            }
+        }
+
+        function mask(event) {
+            var matrix = !this.classList.contains('js-hero-tel') ? "+7 (___) ___ __ __" : "(___) ___ __ __";
+
+            var i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, "");
+
+            if (def.length >= val.length) val = def;
+
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/g.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+            });
+            
+            if (event.type == "blur") {
+                if (this.value.length == 2) this.value = "";
+            } 
+            else{
+                setCursorPosition(this.value.length, this);
+            }
+        };  
+
+        telInputs.forEach(input => {
+            input.addEventListener("input", mask, false);
+            input.addEventListener("focus", mask, false);
+            input.addEventListener("blur", mask, false);
         });
     }
 
@@ -244,11 +341,145 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    window.addEventListener('scroll', scrollHandler);
+    const mousemoveHandler = () => {
+        mouseMoveElements.forEach(element => {
+            const coords = {
+                xCoord: event.clientX,
+                yCoord: event.clientY,
+            }
 
-    dropdownSelectInit(dropdownItems);
-    scrollHandler();
+            for (coord in coords){
+                coords[coord] *= 100;
+
+                if(element.dataset.speed) coords[coord] *= parseFloat(element.dataset.speed);
+
+                if(element.dataset.direction) element.dataset.direction == 'vertical' ? coords.xCoord = 0 : element.dataset.direction == 'horizontal' ? coords.yCoord = 0 : null;
+            }
+
+            if(element.dataset.center == "true"){
+                element.style.transform = `translate( ${(coords.xCoord / window.innerWidth) - 55}%, ${(coords.yCoord / window.innerHeight) - 55}%)`;
+            }
+            else{
+                element.style.transform = `translate( ${coords.xCoord / window.innerWidth}%, ${coords.yCoord / window.innerHeight}%)`;
+            }
+        });
+    }
+
+    const formSubmitHandler = async () => {
+        event.preventDefault();
+
+        const url = '';
+        const targetForm = event.target;
+
+        targetForm.querySelectorAll('input').forEach(input => {
+            if(!input.parentNode.classList.contains('valid')) {
+                input.parentNode.classList.add('invalid');
+                input.addEventListener('input', () => checkInputState(event.target))
+            }
+        });
+
+        if(targetForm.querySelector('.invalid')){
+            targetForm.querySelector('.invalid input').focus();
+            return;
+        }
+
+        const formData = new FormData(targetForm);
+
+        // await fetch(url, {
+        //     method: 'POST',
+        //     body: formData,
+        // })
+        // .then(response => if(response.ok) response.json())
+        // .then(response => {
+        //     closePopup();   
+        //     if(response.status == 1){
+        //         openPopup('.js-popup-thx');
+        //     }
+        //     else{
+        //         openPopup('.js-popup-err');
+        //     }
+        // })
+        // .catch(response => {
+        //     console.log(response);
+        // });
+
+        targetForm.reset();
+        targetForm.querySelectorAll('input').forEach(input => checkInputState(input));
+
+        closePopup();
+        openPopup('.js-popup-thx');
+        setTimeout(closePopup, 1500);
+    }
+
+    const checkInputState = (input) => {
+        input.parentNode.classList.remove('invalid');
+
+        switch (input.getAttribute('type')) {
+            case 'text':
+                input.value.length > 0 ? input.parentNode.classList.add('valid') : input.parentNode.classList.remove('valid');
+                break;
+            case 'email':
+                if(input.value.length > 0 && /\w+@(\w[^@])+\.(\w[^@])+/g.test(input.value)){
+                    input.parentNode.classList.add('valid');
+                }
+                else{
+                    input.parentNode.classList.remove('valid');
+                }
+                break;
+            case 'tel':
+                const inputLength = input.classList.contains('js-hero-tel') ? 15 : 18;
+                
+                input.value.length >= inputLength ? input.parentNode.classList.add('valid') : input.parentNode.classList.remove('valid');
+                break;
+        }
+
+        return input.parentNode.classList.contains('valid');
+    }
+
+    mainForm.querySelectorAll('input').forEach(input => {
+        input.addEventListener('change', () => checkInputState(event.target));
+        input.addEventListener('input', () => checkInputState(event.target));
+        input.addEventListener('blur', () => checkInputState(event.target));
+    });
+
+    popupForm.querySelectorAll('input').forEach(input => {
+        input.addEventListener('change', () => checkInputState(event.target));
+        input.addEventListener('input', () => checkInputState(event.target));
+        input.addEventListener('blur', () => checkInputState(event.target));
+    });
+
+    const openPopup = target => {
+        const targetPopup = document.querySelector(target);
+
+        targetPopup.classList.add('active');
+
+        if(targetPopup.querySelector('input')) targetPopup.querySelector('input').focus();
+    }
+
+    const closePopup = () => {
+        popups.forEach(popup => popup.classList.remove('active'));
+    }
+
+    window.addEventListener('scroll', scrollHandler);
+    document.addEventListener('mousemove', mousemoveHandler);
+
+    forms.forEach(form => form.addEventListener('submit', formSubmitHandler));
+    popupTriggers.forEach(trigger => trigger.addEventListener('click', () => openPopup(trigger.dataset.target)));
+    popupCloseTriggers.forEach(trigger => trigger.addEventListener('click', event => {
+        if(event.target.classList.contains('js-popup-close')) popups.forEach(popup => popup.classList.remove('active'));
+    }));
+
+    document.querySelector('.js-hero-tel').addEventListener('change', () => checkInputState(event.target));
+    document.querySelector('.js-hero-tel').addEventListener('input', () => checkInputState(event.target));
+    document.querySelector('.js-hero-tel').addEventListener('blur', () => checkInputState(event.target));
+
+    countUp();
+    maskInit();
     logosSwiperInit();
+    scrollHandler();
+    mousemoveHandler();
     articlesSwiperInit();
     portfolioSwipersInit();
+    dropdownSelectInit(dropdownItems);
+    
 });
