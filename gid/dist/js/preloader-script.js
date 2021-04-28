8252608ce16c679e96fa2fd143b7c370e93a1676
Belshed <sercom92@gmail.com>
2021-04-28 13:24:59 +0300

    gid renovation done

--- gid/dist/js/preloader-script.js
+++ gid/dist/js/preloader-script.js
@@ -1,28 +1,34 @@
 window.onload = () => {
+    const isDev = false;
+
     const preloader = document.querySelector('.js-preloader');
     const preloaderVideo = preloader.querySelector('.preloader__video');
     const header = document.querySelector('.js-header');
     const logo = header.querySelector('.header__logo');
 
-    const createSource = (path, type) => {
-        const source = document.createElement('source');
-        source.setAttribute('src', path + `.${type}`);
-        source.setAttribute('type', `video/${type}`);
-
-        return source;
-    }
-
     const pathsArr = preloaderVideo.dataset.videos.split(', ');
-    
-    if(!preloaderVideo.childElementCount){
-        const index = Number(Math.random() >= 0.5);
 
-        preloaderVideo.appendChild(createSource(pathsArr[index], 'webm'));
-        preloaderVideo.appendChild(createSource(pathsArr[index], 'mp4'));
+    if(!isDev){    
+        const createSource = (path, type) => {
+            const source = document.createElement('source');
+            source.setAttribute('src', path + `.${type}`);
+            source.setAttribute('type', `video/${type}`);
+    
+            return source;
+        }
+    
+        if(!preloaderVideo.childElementCount){
+            const index = Number(Math.random() >= 0.5);
+    
+            preloaderVideo.appendChild(createSource(pathsArr[index], 'webm'));
+            preloaderVideo.appendChild(createSource(pathsArr[index], 'mp4'));
+        }
+    
+        setTimeout(() => {
+            preloaderVideo.style.maxWidth = logo.getBoundingClientRect().width + 50 + 'px';
+            preloader.classList.add('loaded');
+        }, 2500);
     }
 
-    setTimeout(() => {
-        preloaderVideo.style.maxWidth = logo.getBoundingClientRect().width + 50 + 'px';
-        preloader.classList.add('loaded');
-    }, 2500);
+    preloader.classList.add('loaded');
 };
